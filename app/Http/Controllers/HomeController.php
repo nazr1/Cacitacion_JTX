9f2e1fba838dd98e1c6abdabf18c1f8afef0dc06
Nicolle <nicolle@nicolle-extensa-5230.com>
2023-12-13 08:28:01 -0400

    subi el proyecto

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+use Illuminate\Support\Facades\Auth;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        if(Auth::id())
+        {
+            $usertype=Auth()->user()->usertype;
+
+            if ($usertype=='user')
+            {
+                return view('user.userhome');
+            }
+
+
+            else if ($usertype=='admin')
+            {
+                return view('dashboard');
+            }
+
+            else
+            {
+                return redirect()->back();
+            }
+        }
+
+    }
+}
